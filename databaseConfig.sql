b3b32e343e65b152933c8824197c1bdd836eebc4
a-nai <a-nai@yandex.ru>
2020-12-12 05:28:01 +0300

    Update databaseConfig.sql

--- databaseConfig.sql
+++ databaseConfig.sql
@@ -7,7 +7,7 @@ USE stack_overflow_v2;
       username VARCHAR(255) UNIQUE NOT NULL,
       password VARCHAR(100),
       views INT DEFAULT 0,
-      balance FLOAT DEFAULT 0,
+      balance FLOAT(4),
       created_at TIMESTAMP DEFAULT NOW()
  );
 
@@ -18,7 +18,7 @@ USE stack_overflow_v2;
      views INT DEFAULT 0,
      created_at TIMESTAMP DEFAULT NOW(),
      user_id INT NOT NULL,
-     enlang_id INT NOT NULL,
+     enlang_id INT,
      FOREIGN KEY(user_id) REFERENCES users(id)
  );
 
